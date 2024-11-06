7e360bdfad4f2e69c3afbe89ec9ff583e7c753d8
Abhay Sahani <abhay.cs.777@gmail.com>
2024-11-06 16:02:17 +0530

    reducer.py

--- Big-Data/MapReduce Program 9/reducer.py
+++ Big-Data/MapReduce Program 9/reducer.py
@@ -19,3 +19,10 @@ for line in sys.stdin:
                 max_count = current_count
         current_word = word
         current_count = count
+
+if current_word:
+    if current_count > max_count:
+        max_word = current_word
+        max_count = current_count
+
+print(f"{max_word}\t{max_count}")
